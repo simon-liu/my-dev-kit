770cab0ceed696f7c91b62d76fa4912b1f0407f3
GitHub Action <action@github.com>
2021-04-21 01:00:55 +0000

    Template cleanup

--- src/main/kotlin/com/github/simonliu/mydevkit/MyBundle.kt
+++ src/main/kotlin/com/github/simonliu/mydevkit/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.simonliu.mydevkit
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/simonliu/mydevkit/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/simonliu/mydevkit/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.simonliu.mydevkit.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.simonliu.mydevkit.MyBundle
 
 class MyApplicationService {
 
